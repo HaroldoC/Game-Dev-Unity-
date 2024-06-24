7d4c4c713919d9cd3305e7df559704ad79d8661a
Haroldo Carvalho <carvalhoh@gmail.com>
2024-06-24 18:57:27 -0300

    up

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Game_Manager : MonoBehaviour
+{
+    [SerializeField]
+    private bool _isGameOver;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R) && _isGameOver == true)
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+
+    public void GameOver()
+    {        
+        _isGameOver = true;
+    }
+
+}
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Build.Content;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -13,11 +14,24 @@ public class UIManager : MonoBehaviour
 
     [SerializeField]
     private Sprite[] _liveSprites;
+    [SerializeField]
+    private Text _gameOverText;
+    [SerializeField]
+    private Text _restartLevelText;
+    private Game_Manager _gameManager;
     // Start is called before the first frame update
     void Start()
     {
         // _liveSprites[CurrentPlayerLives = 3]
         _scoreText.text = "Score:" + 0;
+        _gameOverText.gameObject.SetActive(false);
+        _restartLevelText.gameObject.SetActive(false);
+        _gameManager = GameObject.Find("Game_Manager").GetComponent<Game_Manager>();
+
+        if (_gameManager == null)
+        {
+            Debug.LogError("Game Manager is not found in the scene");
+        }
     }
     public void UpdateScore(int playerScore)
     {        
@@ -26,10 +40,30 @@ public class UIManager : MonoBehaviour
     public void UpdateLives(int currentLives)
     {
         _LivesImage.sprite = _liveSprites[currentLives];
-        // if (currentLives == 0)
-        // {
-        //     GameOverSequence();
-        // }
+
+        if (currentLives == 0)
+        {
+            GameOverSequence();                        
+        }
+    }
+
+    void GameOverSequence()
+    {
+        _gameManager.GameOver();
+        _gameOverText.gameObject.SetActive(true);
+        _restartLevelText.gameObject.SetActive(true);
+        StartCoroutine(GameOverFlickerRoutine());                        
+    }
+
+    IEnumerator GameOverFlickerRoutine()
+    {
+        while (true)
+        {
+            _gameOverText.text = "GAME OVER";
+            yield return new WaitForSeconds(0.5f);
+            _gameOverText.text = "";
+            yield return new WaitForSeconds(0.5f);
+        }
     }
 
     // Update is called once per frame
